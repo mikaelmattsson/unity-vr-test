9e23a3a172ea9d5af1b013eceb410c5a919712de
Mikael <mikael.p.mattsson@gmail.com>
2016-11-27 19:35:43 +0100

    Update

--- Assets/Scripts/Objects/PlayerController.cs
+++ Assets/Scripts/Objects/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-	
+        
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/VR/ControllerManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerManager : MonoBehaviour
+{
+
+    private SteamVR_TrackedController device;
+
+    public string logName;
+
+    void Start()
+    {
+        device = transform.GetComponent<SteamVR_TrackedController>();
+        device.TriggerClicked += this.Trigger;
+    }
+
+    void Trigger(object sender, ClickedEventArgs e)
+    {
+        Debug.Log("Trigger" + logName + " has been pressed");
+    }
+}
